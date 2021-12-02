a95f1f5925943dbd4605a4523c86323204d3dcd0
nazzok <65244920+nazzok@users.noreply.github.com>
2021-12-02 11:51:07 -0500

    After some confusion, created Exception classes, added more test data

--- MaraschMorgan15aAssignment/Source.cpp
+++ MaraschMorgan15aAssignment/Source.cpp
@@ -9,42 +9,53 @@ Calculate character offsets
 #include <iostream>
 using namespace std;
 
-
 char character(char, int);
+
+class invalidCharacterException
+	{};
+class invalidRangeException
+	{};
 
 int main() {
 
-	cout << "Test 1: Valid Start and Offset\n";
-	cout << "A offset by 6 is " << character('A', 6);
-	//send valid characters
-	//send non-letter as start
-	//sned out of range
+	//valid
+	cout << "Test 1: A offset by 6\n";
+	cout << character('A', 6) << endl;
+
+	cout << "Test 2: z offset by -1\n";
+	cout << character('z', -1) << endl;
+
+	//invalid range
+	cout << "Test 3: a offset by -1\n";
+	cout << character('a', -1) << endl;
+
+	//invalid character
+	cout << "Test 4: ? offset by 2\n";
+	cout << character('?', 2) << endl;
+
 	return 0;
 }
 
 char character(char start, int offset) {
-	char end;
+	char end = '0';
 	try {
 		if (isalpha(start)) { //if the initial character is a letter
 			end = start + offset;
 			if (isalpha(end)) { //if final character is a letter
 			}
 			else { //if final character isn't a letter
-				char invalidRangeException = end;
-				throw invalidRangeException;
+				throw false;
 			}
 		}
 		else { //if initial character isn't a letter
-			bool invalidCharacterException = start;
-			throw invalidCharacterException;
+			throw invalidCharacterException();
 		}
 		return end;
 	}
-	catch (char invalidCharacterException) {
-		cout << invalidCharacterException << " is not a letter A-Z or a-z";
+	catch (invalidCharacterException) {
+		cout << "invalidCharacterException";
 	}
 	catch (bool invalidRangeException) {
-		cout << "the result is out of range";
+		cout << "invalidRangeException";
 	}
-	return end;
 }
